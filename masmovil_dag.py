88b05c433e83e9e43000aa233b4c937ef8de3cce
ivan.garcia <ivan.garcia@jot-im.com>
2020-11-22 19:18:12 +0100

    Airflow 1) crear dag test

--- masmovil_dag.py
+++ masmovil_dag.py
@@ -1,8 +1,5 @@
 import airflow
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 
 
@@ -35,7 +32,4 @@ dag = DAG(dag_id='test',
 ###############################################################################
 # SCRIPT
 
-start = DummyOperator(task_id='start', dag=dag)
-end = DummyOperator(task_id='end', dag=dag)
 
-start >> end
